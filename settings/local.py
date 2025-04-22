24a4438e63a7dbcde830b8e78c079d56b5faec78
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-22 19:10:35 +0200

    Add proper local.py CORS override

---
+++ settings/local.py
@@ -0,0 +1,9 @@
+# settings/local.py
+from .common import *  # noqa
+
+INSTALLED_APPS += ["corsheaders"]
+MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_METHODS  = ["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
+CORS_ALLOW_HEADERS  = ["Authorization","Content-Type"]
