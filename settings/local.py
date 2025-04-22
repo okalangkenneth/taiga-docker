7f4c60021483095f0f064c1c18797686163bbd8c
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-22 21:03:30 +0200

    Update local.py

--- settings/local.py
+++ settings/local.py
@@ -1,9 +1,32 @@
-# settings/local.py
-from .common import *  # noqa
-
+# Add CORS support
 INSTALLED_APPS += ["corsheaders"]
+
+# Add CORS middleware - it must be placed as high as possible
 MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
-CORS_ALLOW_METHODS  = ["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
-CORS_ALLOW_HEADERS  = ["Authorization","Content-Type"]
+# Allow requests from your React app
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+# For development, you can also enable this (but don't use in production)
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+# Additional headers that might be needed
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-session-id",
+    "x-lazy-pagination",
+    "x-disable-pagination",
+    "set-orders",
+]
