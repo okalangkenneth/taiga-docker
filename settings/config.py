b1df42967b0fbcb982546cc2bd5b537aab1b5097
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-22 17:31:12 +0200

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -3,6 +3,13 @@
 # 1. Pull in all the standard Taiga settings
 from .common import *  # noqa, pylint: disable=unused-wildcard-import
 
+INSTALLED_APPS += ["corsheaders"]
+MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_METHODS = ["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
+CORS_ALLOW_HEADERS = ["Authorization","Content-Type"]
+
+
 #########################################
 # CORS HEADERS (django-cors-headers)
 #########################################
