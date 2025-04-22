6990b8f07eb3bef3cc3b030e59d06d5d94fb0c55
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-22 17:48:00 +0200

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -5,34 +5,9 @@ from .common import *  # noqa, pylint: disable=unused-wildcard-import
 
 INSTALLED_APPS += ["corsheaders"]
 MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
+
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 CORS_ALLOW_METHODS = ["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
 CORS_ALLOW_HEADERS = ["Authorization","Content-Type"]
 
 
-#########################################
-# CORS HEADERS (django-cors-headers)
-#########################################
-
-INSTALLED_APPS += [
-    "corsheaders",
-]
-
-# Must come before CommonMiddleware in settings.common
-MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
-CORS_ALLOW_HEADERS = [
-    "Authorization",
-    "Content-Type",
-]
