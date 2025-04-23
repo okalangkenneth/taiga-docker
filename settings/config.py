424f62566ee104f36b9ee882cda272212aecaa0c
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-23 17:58:33 +0200

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -1,29 +1,12 @@
 # settings/config.py
-# 1) import all the stock Taiga settings
 from .common import *  # noqa
 
-# 2) enable django-cors-headers
-INSTALLED_APPS += [
-    "corsheaders",
-]
-
-# must come before CommonMiddleware
+# Enable django-cors-headers
+INSTALLED_APPS += ["corsheaders"]
 MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
 
-# 3) CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
-CORS_ALLOW_HEADERS = [
-    "Authorization",
-    "Content-Type",
-]
+# CORS rules
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_METHODS = ["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
+CORS_ALLOW_HEADERS = ["Authorization","Content-Type"]
 
