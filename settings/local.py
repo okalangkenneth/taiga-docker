e9931140d5d6c97773b01f9956529d5ccb42bd23
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-23 12:20:35 +0200

    Create local.py

---
+++ settings/local.py
@@ -0,0 +1,16 @@
+cat > settings/local.py << 'EOF'
+from .config import *
+
+# your site-specific overrides go here
+
+# enable django-cors-headers
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+CORS_ALLOW_METHODS = [
+    "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS",
+]
+CORS_ALLOW_HEADERS = [
+    "Authorization", "Content-Type", "X-Requested-With",
+]
+EOF
