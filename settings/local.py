17c2854900b0c0895524518828f38aa8e449ab58
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-23 17:29:11 +0200

    Update local.py

--- settings/local.py
+++ settings/local.py
@@ -1,19 +1,24 @@
-cat > settings/local.py << 'EOF'
-from .config import *
+# settings/local.py
+from .common import *  # noqa
 
-# your site-specific overrides go here
-# django-cors-headers
-INSTALLED_APPS += ["corsheaders"]
+INSTALLED_APPS += [
+    "corsheaders",
+]
+
 MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
 
-# enable django-cors-headers
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 CORS_ALLOW_METHODS = [
-    "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS",
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
 ]
 CORS_ALLOW_HEADERS = [
-    "Authorization", "Content-Type", "X-Requested-With",
+    "Authorization",
+    "Content-Type",
 ]
-EOF
