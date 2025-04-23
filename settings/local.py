117a0e3298d42e77feff467e018f37dcaa5daff3
Kenneth Okalang <okalang.ok@gmail.com>
2025-04-23 17:59:17 +0200

    Update local.py

--- settings/local.py
+++ settings/local.py
@@ -1,24 +1,5 @@
 # settings/local.py
-from .common import *  # noqa
+from .config import *  # noqa
 
-INSTALLED_APPS += [
-    "corsheaders",
-]
-
-MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
-CORS_ALLOW_HEADERS = [
-    "Authorization",
-    "Content-Type",
-]
+# (Optional) any extra “local” tweaks go here…
+# For now, we’ve already pulled in CORS via config.py
